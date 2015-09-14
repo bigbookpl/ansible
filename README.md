e06065f309bb8b84bf4fb3221cc484ed25fe10d5
Krzysztof Konieczny <bigbook.pl@gmail.com>
2015-09-14 23:20:58 +0200

    Added desktop configuration

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # chef
+
+Co ma być instalowane (must be)
+
+- mc
+- nload
+- htop
+- git
+- guake
+- zsh
+
+- ohmyzsh
+- openjdk
+
+- chromium
+- atom
+- intellij idea
+- docker
+
+Nice to have
+Ustawić tapety
---
+++ packages.rb
@@ -0,0 +1,38 @@
+user = 'bigbook'
+
+package 'mc'
+package 'nload'
+package 'htop'
+package 'git'
+package 'curl'
+
+package 'guake' do
+  notifies :run, 'execute[set-gauke-startup]', :immediately
+end
+
+execute 'set-gauke-startup' do
+  command 'cp /usr/share/applications/guake.desktop /etc/xdg/autostart/'
+end
+
+template '/home/bigbook/xxs' do
+    source 'README.md'
+    mode 0755
+    action :create_if_missing
+end
+
+package 'zsh' do
+  notifies :run, 'execute[download-ohmyzhs]', :immediately
+end
+
+execute 'download-ohmyzhs' do
+  command "curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
+  command 'chsh -s /bin/zsh'
+  retries 5
+end
+
+package 'openjdk-7-jdk'
+package 'chromium-browser'
+
+directory "/home/#{user}/dev" do
+  action :create
+end
